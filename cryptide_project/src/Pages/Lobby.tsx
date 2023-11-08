212b68b2bf00b817e24f17f32d1046f9ccc3cc08
Pierre Ferreira <p.ferreira1@outlook.fr>
2023-11-08 14:01:44 +0100

    finission de la page Lobby (sans logique) :adhesive_bandage:

--- cryptide_project/src/Pages/Lobby.tsx
+++ cryptide_project/src/Pages/Lobby.tsx
@@ -13,7 +13,7 @@ function Lobby() {
             <div className='left-part'>
                 <div className='player-board'>
                     {/* //! voir pour la gestion avec un liste, utilisateur avec le "+ (vous)" et les pdp avec les lettres grecs (?)*/}
-                    <PlayerItemList pdp={Person} name="Dummy"/>
+                    <PlayerItemList pdp={Person} name="Dummy (vous)"/>
                     <PlayerItemList pdp={Bot} name="Boat"/>
                     <PlayerItemList pdp={Bot} name="Bot-tom"/>
                 </div>
@@ -27,12 +27,16 @@ function Lobby() {
                     <h2>Paramètre de la partie</h2>
                 </div>
                 <ul>
-                    <li><h4>paramètre surper important pour la partieparamètre surper important pour la partieparamètre surper important pour la partieparamètre surper important pour la partieparamètre surper important pour la partie</h4></li>
-                    <li><h4>paramètre surper important pour la partie</h4></li>
-                    <li><h4>paramètre surper important pour la partie</h4></li>
-                    <li><h4>paramètre surper important pour la partie</h4></li>
-                    <li><h4>paramètre surper important pour la partie</h4></li>
-                    <li><h4>paramètre surper important pour la partie</h4></li>
+                    <li><h4> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim</h4></li>
+                    <li><h4>paramètre super important pour la partie</h4></li>
+                    <li><h4>paramètre super important pour la partie</h4></li>
+                    <li><h4>paramètre super important pour la partie</h4></li>
+                    <li><h4>Niveau des bots : Facile </h4></li> {/* mettre un dropdown ou un swiper */}
+                    <li><h4>Thèmes : basique </h4></li> {/* mettre un dropdown*/}
+                    {
+                        //? mettre un timer pour chaques personne ?
+                        //? indice avancé ? ==> négation, voisin du 2e degré etc.
+                    }
                 </ul>
                 <center>
                     <ButtonImgNav dest='/' img={cible} text=' À la chasse !'/> {/* page de baptiste ici */}
