202c304abde3229ebc23e6125e8eb7e74e6bc4cf
Pierre Ferreira <p.ferreira1@outlook.fr>
2023-11-14 09:59:12 +0100

    ajout des drapeaux dans pour les langages :package:

--- cryptide_project/src/Components/NavBar.tsx
+++ cryptide_project/src/Components/NavBar.tsx
@@ -12,9 +12,11 @@ import { BsFillPersonPlusFill } from 'react-icons/bs';
 import { HiLanguage } from 'react-icons/hi2';
 
 /* Components */
-import './NavBar.css';
+import ReactCountryFlag from "react-country-flag"
+
 
 /* Style */
+import './NavBar.css';
 import { useTheme } from '../Style/ThemeContext';
 
 // @ts-ignore
@@ -55,9 +57,23 @@ function AppNavbar({changeLocale}) {
                     title={<span><HiLanguage /></span>}
                     className="navbar-title" id="basic-nav-dropdown">
                         <NavDropdown.Item onClick={() => changeLocale('fr')}> 
+                            <ReactCountryFlag countryCode="FR"
+                                                svg
+                                                style={{
+                                                    width: '30px',
+                                                    height: '20px',
+                                                    margin: 'auto 10px 3px auto',
+                                                }}/>        
                             <FormattedMessage id="languageSelector.french"/> 
                         </NavDropdown.Item>
                         <NavDropdown.Item onClick={() => changeLocale('en')}> 
+                            <ReactCountryFlag countryCode="GB" 
+                                                svg
+                                                style={{
+                                                    width: '30px',
+                                                    height: '20px',
+                                                    margin: 'auto 10px 3px auto',
+                                                }}/>
                             <FormattedMessage id="languageSelector.english"/> 
                         </NavDropdown.Item>
                     </NavDropdown>
