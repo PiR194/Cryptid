55f2a35414c561d511a4ab65cd6b0dc357eecac5
Thomas Chazot <tchazot63@gmail.com>
2023-11-15 08:12:21 +0100

    mastermind fini

--- cryptide_project/src/Components/GraphContainer.tsx
+++ cryptide_project/src/Components/GraphContainer.tsx
@@ -32,10 +32,16 @@ let lastAskingPlayer = 0
 let lastNodeId = -1
 let first = true
 let askedWrong = false
+let solo: boolean = true
+
 
 const MyGraphComponent: React.FC<MyGraphComponentProps> = ({onNodeClick, handleShowTurnBar, handleTurnBarTextChange}) => {
 
   const { indices, indice, person, personNetwork, setNodeIdData, players, askedPersons, setActualPlayerIndexData, room, actualPlayerIndex, turnPlayerIndex, onlyFalse, setOnlyFalseData } = useGame();
+
+  const params = new URLSearchParams(window.location.search);
+  const solotmp = params.get('solo');
+
 
   let playerIndex: number = turnPlayerIndex
   let index = 0
@@ -50,15 +56,16 @@ const MyGraphComponent: React.FC<MyGraphComponentProps> = ({onNodeClick, handleS
 
   if (first){
     first = false
-    setActualPlayerIndexData(index)
-    if (playerIndex == thisPlayerIndex){
-      handleTurnBarTextChange("À vous de jouer")
-      handleShowTurnBar(true)
-
+    if (solotmp == "false"){
+      solo=false
     }
-    indices.forEach(i => {
-      console.log(i.ToString("en"))
-    });
+    if (!solo){
+      setActualPlayerIndexData(index)
+      if (playerIndex == thisPlayerIndex){
+        handleTurnBarTextChange("À vous de jouer")
+        handleShowTurnBar(true)
+      }
+    }
   }
 
   useEffect(() => {
@@ -72,7 +79,6 @@ const MyGraphComponent: React.FC<MyGraphComponentProps> = ({onNodeClick, handleS
       console.error("Container not found");
       return;
     }
-
     // Charger les données dans le graph
     const nodes = new DataSet(graph.nodesPerson);
 
@@ -99,84 +105,86 @@ const MyGraphComponent: React.FC<MyGraphComponentProps> = ({onNodeClick, handleS
     
     const networkData = { nodes: nodes, edges: graph.edges };
     const network = new Network(container, networkData, initialOptions);
-
-    socket.on("asked all", (id) =>{
-      const pers = personNetwork.getPersons().find((p) => p.getId() == id)
-      if (pers!=undefined){
-        askedPersons.push(pers)
-      }
-    })
-    
-    socket.on("node checked",(id, works, color, newPlayerIndex) => {
-      const node = nodes.get().find((n) => id == n.id)
-      if (node!=undefined){
-        onNodeClick(false)
-        playerIndex = newPlayerIndex
-        if (!node.label.includes(colorToEmoji(color, works))){
-          networkData.nodes.update({id: id, label: node.label + colorToEmoji(color, works)})
-        }
-        if (playerIndex === thisPlayerIndex){
-          handleTurnBarTextChange("À vous de jouer")
-          handleShowTurnBar(true)
-        }
-        else{
-          handleShowTurnBar(false)
-        }
-      }
-      lastAskingPlayer = 0
-      lastNodeId = -1
-    })
-
-    socket.on("already asked", (nodeId, askedPlayer) =>{
-      console.log("player: " + askedPlayer + " already asked on node " + nodeId)
-    })
-
-    socket.on("asked wrong", () =>{
-      setOnlyFalseData(true)
-      askedWrong = true
-      handleShowTurnBar(true)
-      handleTurnBarTextChange("Mauvais choix, posez un carré !")
-    })
-
-
-    socket.on("asked", (nodeId, askingPlayer, askingPlayerIndex) => {
-      console.log(askingPlayerIndex)
-      if (askingPlayer.id !== lastAskingPlayer || nodeId !== lastNodeId ){
-        lastAskingPlayer = askingPlayer.id
-        lastNodeId = nodeId
-        const pers = personNetwork.getPersons().find((p) => p.getId() == nodeId)
+    if (!solo){
+      socket.on("asked all", (id) =>{
+        const pers = personNetwork.getPersons().find((p) => p.getId() == id)
         if (pers!=undefined){
-          if (askedPersons.includes(pers)){
-            socket.emit("already asked", nodeId, askingPlayer, socket.id)
-            return
+          askedPersons.push(pers)
+        }
+      })
+      
+      socket.on("node checked",(id, works, color, newPlayerIndex) => {
+        const node = nodes.get().find((n) => id == n.id)
+        if (node!=undefined){
+          onNodeClick(false)
+          playerIndex = newPlayerIndex
+          if (!node.label.includes(colorToEmoji(color, works))){
+            networkData.nodes.update({id: id, label: node.label + colorToEmoji(color, works)})
+          }
+          if (playerIndex === thisPlayerIndex){
+            handleTurnBarTextChange("À vous de jouer")
+            handleShowTurnBar(true)
           }
           else{
-            askedPersons.push(pers)
-            const node = nodes.get().find((n) => nodeId == n.id)
-            if (node != undefined && indice != null){
-              var tester = IndiceTesterFactory.Create(indice)
-              let maybe = thisPlayerIndex
-              playerIndex = playerIndex + 1
-              if(playerIndex == players.length){
-                playerIndex = 0
-              }
-              if (tester.Works(pers)){
-                socket.emit("node checked", nodeId, true, positionToColor(thisPlayerIndex), room, playerIndex)
-              }
-              else{
-                maybe = maybe - 1
-                if(maybe == 0){
-                  maybe = players.length - 1
-                }
-                socket.emit("node checked", nodeId, false, positionToColor(thisPlayerIndex), room, maybe)
-                socket.emit("asked wrong", askingPlayer, room)
-              }
-            }
-          }     
+            handleShowTurnBar(false)
+          }
         }
-      }
-      
-    })
+        lastAskingPlayer = 0
+        lastNodeId = -1
+      })
+  
+      socket.on("already asked", (nodeId, askedPlayer) =>{
+        console.log("player: " + askedPlayer + " already asked on node " + nodeId)
+      })
+  
+      socket.on("asked wrong", () =>{
+        setOnlyFalseData(true)
+        askedWrong = true
+        handleShowTurnBar(true)
+        handleTurnBarTextChange("Mauvais choix, posez un carré !")
+      })
+  
+  
+      socket.on("asked", (nodeId, askingPlayer, askingPlayerIndex) => {
+        console.log(askingPlayerIndex)
+        if (askingPlayer.id !== lastAskingPlayer || nodeId !== lastNodeId ){
+          lastAskingPlayer = askingPlayer.id
+          lastNodeId = nodeId
+          const pers = personNetwork.getPersons().find((p) => p.getId() == nodeId)
+          if (pers!=undefined){
+            if (askedPersons.includes(pers)){
+              socket.emit("already asked", nodeId, askingPlayer, socket.id)
+              return
+            }
+            else{
+              askedPersons.push(pers)
+              const node = nodes.get().find((n) => nodeId == n.id)
+              if (node != undefined && indice != null){
+                var tester = IndiceTesterFactory.Create(indice)
+                let maybe = thisPlayerIndex
+                playerIndex = playerIndex + 1
+                if(playerIndex == players.length){
+                  playerIndex = 0
+                }
+                if (tester.Works(pers)){
+                  socket.emit("node checked", nodeId, true, positionToColor(thisPlayerIndex), room, playerIndex)
+                }
+                else{
+                  maybe = maybe - 1
+                  if(maybe == 0){
+                    maybe = players.length - 1
+                  }
+                  socket.emit("node checked", nodeId, false, positionToColor(thisPlayerIndex), room, maybe)
+                  socket.emit("asked wrong", askingPlayer, room)
+                }
+              }
+            }     
+          }
+        }
+        
+      })
+    }
+    
 
 
     
@@ -209,32 +217,44 @@ const MyGraphComponent: React.FC<MyGraphComponentProps> = ({onNodeClick, handleS
       
       if(params.nodes.length > 0){
         setNodeIdData(params.nodes[0])
-        // Renvoyer un true pour afficher la choice bar
-        if (askedWrong){
-          console.log(askedWrong)
-          const person = personNetwork?.getPersons().find((p) => p.getId() == params.nodes[0])
-          if (person !== undefined && indice !== null){
-            const tester = IndiceTesterFactory.Create(indice)
-            if (!tester.Works(person) && !askedPersons.includes(person)){
-              console.log(playerIndex)
-              playerIndex = thisPlayerIndex + 1
-              if(playerIndex == players.length){
-                playerIndex = 0
+        if (!solo){
+          if (askedWrong){
+            const person = personNetwork?.getPersons().find((p) => p.getId() == params.nodes[0])
+            if (person !== undefined && indice !== null){
+              const tester = IndiceTesterFactory.Create(indice)
+              if (!tester.Works(person) && !askedPersons.includes(person)){
+                playerIndex = thisPlayerIndex + 1
+                if(playerIndex == players.length){
+                  playerIndex = 0
+                }
+                socket.emit("node checked", params.nodes[0], false, positionToColor(thisPlayerIndex), room, playerIndex)
+                askedPersons.push(person)
+                askedWrong = false
               }
-              socket.emit("node checked", params.nodes[0], false, positionToColor(thisPlayerIndex), room, playerIndex)
-              askedPersons.push(person)
-              askedWrong = false
             }
           }
-          
-        }
-        else if (thisPlayerIndex == playerIndex){
-          onNodeClick(true)
+          else if (thisPlayerIndex == playerIndex){
+            onNodeClick(true)
+          }
+          else{
+            onNodeClick(false)
+          }
         }
         else{
-          onNodeClick(false)
+          const person = personNetwork?.getPersons().find((p) => p.getId() == params.nodes[0]) //person sélectionnée
+          if (person != undefined){
+            indices.forEach(async (i, index) =>{
+              const tester = IndiceTesterFactory.Create(i)
+              const test = tester.Works(person)
+              const node = nodes.get().find((n) => params.nodes[0] == n.id)
+              if (node!=undefined){
+                networkData.nodes.update({id: params.nodes[0], label: node.label + colorToEmoji(positionToColor(index), test)})
+              }
+            })
+          }
         }
       }
+        // Renvoyer un true pour afficher la choice bar
       else{
         // Renvoyer un false pour cacher la choice bar
         onNodeClick(false)
@@ -247,6 +267,10 @@ const MyGraphComponent: React.FC<MyGraphComponentProps> = ({onNodeClick, handleS
       <div id="graph-container"/>
     </>
   );
+
+  function delay(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
 }
 
 
