4996a78ceac1c0154e8b02a0c0c679cc8a7918c2
Pierre Ferreira <p.ferreira1@outlook.fr>
2023-11-20 10:48:50 +0100

    nettoyage des fichiers de config jest, je laisse cependant les dependancesde jest et de babel lié :package:

--- cryptide_project/babel-css-transformer.js
+++
@@ -1,7 +0,0 @@
-// babel-css-transformer.js
-const babelJest = require('babel-jest');
-
-module.exports = babelJest.createTransformer({
-  // Configuration pour Babel, par exemple :
-    presets: ['@babel/preset-env'],
-});
--- cryptide_project/empty-module.js
+++
@@ -1,2 +0,0 @@
-// empty-module.js
-module.exports = {};
