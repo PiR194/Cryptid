16de4b4be7c6b4d47713d8f7f3f41313806cfc25
Pierre Ferreira <p.ferreira1@outlook.fr>
2023-11-20 09:28:35 +0100

    ajout de jest.config.ts et de dependance :package:

---
+++ cryptide_project/jest.config.ts
@@ -0,0 +1,24 @@
+// import type {Config} from 'jest';
+
+// const config: Config = {
+//     verbose: true,
+// };
+
+// export default config;
+
+// import type {Config} from 'jest';
+// import {defaults} from 'jest-config';
+
+// const config: Config = {
+//     moduleFileExtensions: [...defaults.moduleFileExtensions, 'mts'],
+// };
+
+// export default config;
+
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+    moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+};
+
+export default config;
