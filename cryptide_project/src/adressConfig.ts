87b6cdb8f4e870859440de061780942287eafa69
Pierre Ferreira <p.ferreira1@outlook.fr>
2023-11-28 09:33:25 +0100

    ajout

--- cryptide_project/src/adressConfig.ts
+++ cryptide_project/src/adressConfig.ts
@@ -2,6 +2,6 @@ const ADRESSE_WEBSERVER = "http://localhost:3002"
 
 const ADRESSE_DBSERVER = "http://localhost:3003"
 
-const ADRESSE_WEBSITE = "http://localhost:3000"
+const ADRESSE_WEBSITE = ""
 
 export {ADRESSE_DBSERVER, ADRESSE_WEBSERVER, ADRESSE_WEBSITE}
