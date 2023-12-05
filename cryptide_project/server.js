5a6e49b6796102c8d50e1c0371f87a10eb39aba3
Thomas CHAZOT <thomas.chazot2@etu.uca.fr>
2023-12-05 09:44:37 +0100

    Mise à jour de 'cryptide_project/server.js'

--- cryptide_project/server.js
+++ cryptide_project/server.js
@@ -8,8 +8,14 @@ const port = process.env.PORT || 80;
 app.use(express.static(path.join(__dirname, 'build')));
 
 // Définir le type MIME pour les fichiers JavaScript
-app.use('*.js', (req, res, next) => {
+app.use('/static/js', (req, res, next) => {
   res.type('application/javascript; charset=utf-8');
+  next();
+});
+
+// Définir le type MIME pour les fichiers CSS
+app.use('/static/css', (req, res, next) => {
+  res.type('text/css; charset=utf-8');
   next();
 });
 
