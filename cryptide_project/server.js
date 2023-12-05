cbf548d9a04f3ad74bf1de844ad1b879e41cca52
Thomas CHAZOT <thomas.chazot2@etu.uca.fr>
2023-12-05 09:58:39 +0100

    Mise à jour de 'cryptide_project/server.js'

--- cryptide_project/server.js
+++ cryptide_project/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 80;
 
 // Servir les fichiers statiques depuis le dossier 'build'
 app.use(express.static(path.join(__dirname, 'build')));
