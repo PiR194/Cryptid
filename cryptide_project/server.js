05d1a7bf6bb80812a52608e1b690afa2d1f11e66
Thomas CHAZOT <thomas.chazot2@etu.uca.fr>
2023-12-05 10:51:51 +0100

    Mise à jour de 'cryptide_project/server.js'

--- cryptide_project/server.js
+++ cryptide_project/server.js
@@ -5,27 +5,27 @@ const app = express();
 const port = process.env.PORT || 80;
 
 // Servir les fichiers statiques depuis le dossier 'build'
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname), "build"));
 
 // Définir le type MIME pour les fichiers JavaScript
 app.use('/static/js', (req, res, next) => {
   res.type('application/javascript; charset=utf-8');
   next();
-}, express.static(path.join(__dirname, 'static/js')));
+}, express.static(path.join(__dirname, '"build"/static/js')));
 
 // Définir le type MIME pour les fichiers CSS
 app.use('/static/css', (req, res, next) => {
   res.type('text/css; charset=utf-8');
   next();
-}, express.static(path.join(__dirname, 'static/css')));
+}, express.static(path.join(__dirname, '"build"/static/css')));
 
 // Route par défaut pour servir l'application React
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, '"build"/index.html'));
 });
 
 // Démarrer le serveur
 app.listen(port, () => {
   console.log(`Serveur en cours d'exécution sur le port ${port}`);
-  console.log(path.join(__dirname, 'static/css'))
+  console.log(path.join(__dirname, '"build"/static/css'))
 });
