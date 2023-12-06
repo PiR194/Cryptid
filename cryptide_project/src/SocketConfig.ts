476129c01ed0e485f79a608878d583b1d1067859
Thomas CHAZOT <thomas.chazot2@etu.uca.fr>
2023-12-06 10:36:43 +0100

    Mise à jour de 'cryptide_project/src/SocketConfig.ts'

--- cryptide_project/src/SocketConfig.ts
+++ cryptide_project/src/SocketConfig.ts
@@ -1,7 +1,13 @@
 import { io } from "socket.io-client";
 import { ADRESSE_WEBSERVER } from "./AdressSetup";
 
-const socket = io(ADRESSE_WEBSERVER);
+const socket = io(ADRESSE_WEBSERVER, {
+  withCredentials: true,
+  extraHeaders: {
+    'Access-Control-Allow-Origin': 'https://codefirst.iut.uca.fr/containers/Crypteam-website:80', // Replace with your client's domain and port
+    'Access-Control-Allow-Credentials': 'true'
+  }
+});
 
 console.log(ADRESSE_WEBSERVER)
 console.log(socket.id)
