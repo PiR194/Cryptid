cdb262ca5f2b40f42a070d712ab2559c917a11bb
Thomas CHAZOT <thomas.chazot2@etu.uca.fr>
2023-12-07 09:33:13 +0100

    Mise à jour de 'cryptide_project/src/AdressSetup.ts'

--- cryptide_project/src/AdressSetup.ts
+++ cryptide_project/src/AdressSetup.ts
@@ -1,4 +1,4 @@
-const ADRESSE_WEBSERVER = "https://codefirst.iut.uca.fr/containers/Crypteam-server:80"
+const ADRESSE_WEBSERVER = "https://codefirst.iut.uca.fr:80"
 
 const ADRESSE_DBSERVER = "http://172.20.10.4:3003"
 
