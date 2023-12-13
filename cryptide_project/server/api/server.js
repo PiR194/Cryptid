aa795d1d617d7612b88685434303b4edc9dbf1d3
Thomas CHAZOT <thomas.chazot2@etu.uca.fr>
2023-12-13 11:37:30 +0100

    Mise à jour de 'cryptide_project/server/api/server.js'

--- cryptide_project/server/api/server.js
+++ cryptide_project/server/api/server.js
@@ -42,7 +42,7 @@ app.use((err, req, res, next) => {
 
 // Middleware pour la gestion des erreurs 404
 app.use((req, res) => {
-  console.log(req)
+  console.log(req.url)
   res.status(404).send('Page non trouvée');
 });
 
