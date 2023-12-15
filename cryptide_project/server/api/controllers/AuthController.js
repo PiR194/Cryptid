f606711cb6666db2a97507786675af9ae578eecb
Thomas Chazot <tchazot63@gmail.com>
2023-12-15 12:32:16 +0100

    fix pb api

--- cryptide_project/server/api/controllers/AuthController.js
+++ cryptide_project/server/api/controllers/AuthController.js
@@ -187,7 +187,7 @@ class AuthController {
         try{
             await db.connect();
 
-            const [users, fields] = await db.getUsers();
+            const users = await db.getUsers()[0];
             console.log(users)
 
             res.status(200).json({ tab : users });
@@ -207,7 +207,7 @@ class AuthController {
         try{
             await db.connect();
 
-            const [users, fields] = await db.getGames();
+            const users = await db.getGames()[0];
             console.log(users)
 
             res.status(200).json({ tab : users });
