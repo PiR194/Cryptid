cb6a5406737363785faa9a54534301b068c742e0
Thomas CHAZOT <thomas.chazot2@etu.uca.fr>
2023-12-20 18:45:35 +0100

    Mise à jour de 'cryptide_project/src/AdressSetup.ts'

--- cryptide_project/src/AdressSetup.ts
+++ cryptide_project/src/AdressSetup.ts
@@ -6,7 +6,7 @@ const ADRESSE_DBSERVER = "https://codefirst.iut.uca.fr/containers/Crypteam-api"
 const tmp = ADRESSE_DBSERVER
 const tmp2 = ADRESSE_WEBSERVER
 
-console.log(ADRESSE_WEBSERVER)
+console.log(ADRESSE_WEBSERVER + " wesh")
 
 const ADRESSE_WEBSITE = ""
 
